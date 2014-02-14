1f4d7e588cfcec6b44a2b9146aa6fb4ef7a2a0aa
Jordan Ranz <jranz@hotmail.com>
2014-02-14 12:21:18 -0800

    Added sign up and user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Spots::Application.routes.draw do
+  get "users/new"
+
   root 'static#home'
+  match '/signup', to: 'users#new', via: 'get'
   match '/about', to: 'static#about', via: 'get'
   match '/contact', to: 'static#contact', via: 'get'
 
