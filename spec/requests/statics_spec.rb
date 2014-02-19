363b8a72c8345f44aeabe761b62ceec868b8decf
Jordan Ranz <jranz@hotmail.com>
2014-02-19 15:25:37 -0800

    Finished modeling test framework

--- spec/requests/statics_spec.rb
+++ spec/requests/statics_spec.rb
@@ -13,11 +13,11 @@ describe "Statics" do
   describe "About page" do
     before { visit about_path }
     
-    it { should have_title(full_title('About Us')) }
+    it { should have_title(full_title('About')) }
   end
 
   describe "Contact page" do
-    before { visit about_path }
+    before { visit contact_path }
     
     it { should have_title(full_title('Contact')) }
   end
