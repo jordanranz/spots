a07ca4d74130478042da7fd2530d7d31861d6d9a
Jordan Ranz <jranz@hotmail.com>
2014-02-20 01:46:34 -0800

    Added Sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Spots::Application.routes.draw do
-  get "users/new"
-
+  resources :users
   root 'static#home'
   match '/signup', to: 'users#new', via: 'get'
   match '/about', to: 'static#about', via: 'get'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Jordan Ranz"
+    email "jordanmranz@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
