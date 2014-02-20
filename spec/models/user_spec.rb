b3b31c233ddc65238907c8733506d7e51acdead9
Jordan Ranz <jranz@hotmail.com>
2014-02-19 22:19:15 -0800

    Made basic user model with secure password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,8 @@ describe User do
 
   it { should be_valid }
 
+  it { should respond_to(:authenticate) }
+
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
@@ -70,5 +72,26 @@ describe User do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
+
+  describe "with a password that is too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) } 
+  
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end 
 
 end
